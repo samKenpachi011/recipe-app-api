b0b4bf5dd4b057eaea30ef32f8a99906999d9f9e
samKenpachi011 <samuelkabelo1@gmail.com>
2023-03-02 11:51:42 +0200

    ADD: recipe model tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
-from django.db import models # noqa
+from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -39,3 +40,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,9 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from decimal import Decimal
+from core import models
+
 
 class ModelTests(TestCase):
     """Test models"""
@@ -42,3 +45,22 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe desc'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
